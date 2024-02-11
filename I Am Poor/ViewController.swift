25feac67ff5f3d9ddf8de0208d8a1c9c1ecabc64
Андрей Линьков <103899191+Otarkush@users.noreply.github.com>
2024-02-11 12:21:45 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Андрей Линьков on 07.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
